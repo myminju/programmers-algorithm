9bf02645c81855828f75f873798ec8e05732fc85
myminju <mjeon7890@gmail.com>
2021-07-11 18:59:03 +0900

    first commit

---
+++ hash/Lv1_NotCompletedPlayer.java
@@ -0,0 +1,14 @@
+package hash;
+
+import java.util.Arrays;
+
+public class Lv1_NotCompletedPlayer {
+    public static void main(String[] args) {
+
+    }
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        return answer;
+    }
+}
